375110c4b756fdaf820fc3fe2bcb65d3f1c838d9
Kevin Santo Cappuccio <KevinC@ppucc.io>
2024-08-31 10:17:08 -0700

    parse markdown

--- docs/conf.py
+++ docs/conf.py
@@ -14,12 +14,16 @@ release = '5.0.0.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
