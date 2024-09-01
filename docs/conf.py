d5bc60944b811c8bfcab131582162ee76175ec08
Kevin Santo Cappuccio <KevinC@ppucc.io>
2024-08-31 17:54:03 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_favicon = 'favicon.ico'
+bodyfont = 'Monaco'
+headfont = 'Monaco'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
