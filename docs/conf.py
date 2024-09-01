bbcd588d4d75e73f90e204455e720247e197fc12
Kevin Santo Cappuccio <KevinC@ppucc.io>
2024-08-31 17:43:22 -0700

    favicon

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+html_favicon = 'favicon.ico'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
